14393a59855ab4ccc725549ceb2780b760e502a5
Felipe Coelho <felipecrleandro@gmail.com>
2023-10-18 14:14:10 -0300

    home api, fix auth flow

---
+++ src/app/api/home/route.ts
@@ -0,0 +1,29 @@
+import { PubsProps } from '@/app/(logged)/admin/pubs/columns'
+
+interface FetchPubsProps {
+  per_page: number
+  total: number
+  current_page: number
+  data: PubsProps[]
+}
+
+export async function getData(
+  token: string | undefined
+): Promise<FetchPubsProps> {
+  const res = await fetch('http://localhost:3333/pubs', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
+    },
+    next: { revalidate: 0 }
+  })
+
+  if (!res.ok) {
+    // redirect to 500 page
+  }
+
+  const data = await res.json()
+
+  return data
+}
